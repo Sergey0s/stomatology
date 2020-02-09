d051c60ec2669ad4fac9f03e95949d4318186d2b
Sergey0s <mediaworld@mail.ru>
2020-02-09 22:39:55 +0300

    fixed firstEntry test

--- src/components/Patient/Patient.js
+++ src/components/Patient/Patient.js
@@ -5,24 +5,38 @@ import {connect} from "react-redux";
 class Patient extends Component {
 
     render() {
-        // console.log(this.props.firstEntry)
-        console.log()
+        const entryTest = this.props.firstEntry[this.props.id].patientData.entryTest;
+        let updatedEntryTest = [];
+        if (entryTest) {
+            for (let key in entryTest) {
+                updatedEntryTest.push(entryTest[key])
+            }
+            console.log(updatedEntryTest)
+        }
         return (
             <div className={classes.Patient}>
-                <p>ФИО: {this.props.surname} {this.props.name} {this.props.secondName} </p>
-                <p>Дата регистрации: {new Date(this.props.registerDate).toLocaleString()}</p>
+                <p className={classes.Patient__p}>ФИО: {this.props.surname} {this.props.name} {this.props.secondName} </p>
+                <p className={classes.Patient__p}>Дата
+                    регистрации: {new Date(this.props.registerDate).toLocaleString()}</p>
                 {
-                    !this.props.firstEntry[this.props.id].patientData.entryTest &&
-                    <button onClick={this.props.firstEntryHandler}>Первичный прием</button>
+                    !entryTest &&
+                    <button className={classes.Patient__firstEntryButton} onClick={this.props.firstEntryHandler}>Первичный прием</button>
+                    ||
+                    <div>
+                        <p className={classes.Patient__p}> Результаты первого приема: </p>
+                        <p className={classes.Patient__p}> Жалобы: {updatedEntryTest[0].complaints.value} </p>
+                        <p className={classes.Patient__p}> Диагноз: К 12.0 - рецидивирующие афты полости рта</p>
+                    </div>
                 }
             </div>
-        )};
-    }
+        )
+    };
+}
 
-    const mapStateToProps = (state) => {
+const mapStateToProps = (state) => {
     return {
         firstEntry: state.patientData.patients
     }
-    };
+};
 
-    export default connect(mapStateToProps)(Patient);
+export default connect(mapStateToProps)(Patient);
